d838268f4b50a0f7af2f6bb15f468bc4ab1e03d8
acu990712 <acu990712@gmail.com>
2020-11-01 15:41:51 -0500

    enhancing program

--- listPairSum.py
+++ listPairSum.py
@@ -1,8 +1,8 @@
 def pair_sum(a, b):   
     if len(a)<2:
-        return
+        return 0
     seen = set()
-    print type(seen)
+    #print(type(seen))
     #output = set()
     count =0
     for x in a:
@@ -13,7 +13,12 @@ def pair_sum(a, b):
             #output.add( ( (min(x,target)),max(x,target) ) )
             seen.remove(target)
             count+=1
-
     return count
 suma=pair_sum ( [1,2,3,4,4,1,4,1,1,1,1,3,3,2,2,4,4,4],5)
-print suma
+print(suma)
+suma=pair_sum ( [1,2,3,4,4,1,4,1,1,1,1,3,3,2,2,4,4,4],6)
+print(suma)
+suma=pair_sum ( [1,2,3,4,4,1,4,1,1,1,1,3,3,2,2,4,4,4],4)
+print(suma)
+suma=pair_sum ( [1],1)
+print(suma)
