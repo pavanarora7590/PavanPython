e76209485c627352a7eaef213f495a8963cead00
acu990712 <acu990712@gmail.com>
2020-11-01 11:42:39 -0500

    fix all print as per python3 format

--- DtIntStrTup.py
+++ DtIntStrTup.py
@@ -2,22 +2,22 @@ a = 5
 print (type(a))
 
 a = 12
-print a
+print (a)
 
 
-print 'string immutable so can only replace them but cant change a part of string, can refer to elements of tuple via index'
+print ('string immutable so can only replace them but cant change a part of string, can refer to elements of tuple via index')
 b = 'hello'
-print b[1]
-print type(b)
+print (b[1])
+print (type(b))
 b = b.replace('e', 'f')
-print b
-print 'tuple immutable so can only replace them but cant change a part of string'
-print 'tuple can create another tuple which is sub part of one, or can refer to elements of tuple via index'
+print (b)
+print ('tuple immutable so can only replace them but cant change a part of string')
+print ('tuple can create another tuple which is sub part of one, or can refer to elements of tuple via index')
 c = (1,2,'fg')
-print type(c)
-print c[0], c
+print (type(c))
+print (c[0], c)
 c = c[0:2]
-print type(c)
+print (c,type(c))
 
 
 
